f4925bfbf1dbeca6782360763dc49a9a80260326
ddomen <daniele.domenichelli.5@gmail.com>
2019-06-21 17:50:52 +0200

    documentazione UString

--- src/main/java/Utils/UString.java
+++ src/main/java/Utils/UString.java
@@ -6,7 +6,18 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 public final class UString {
+    /**
+     * Rimuove i caratteri unicode dannosi per le chiamate http
+     * @param string stringa da sanare
+     * @return stringa sanata
+     */
     public static String Escape(String string){ return StringEscapeUtils.unescapeJava(string); }
 
+    /**
+     * Codifica una stringa in formato utf-8
+     * @param string stringa da codificare
+     * @return string codificata
+     * @throws UnsupportedEncodingException
+     */
     public static String Utf8Encode(String string) throws UnsupportedEncodingException { return URLEncoder.encode(string, "UTF-8"); }
 }
