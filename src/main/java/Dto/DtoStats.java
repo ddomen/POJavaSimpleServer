c0f5490c62ed22953a1f930f1fba079a1ff3267c
ddomen <daniele.domenichelli.5@gmail.com>
2019-06-21 11:24:10 +0200

    preparate statistiche

---
+++ src/main/java/Dto/DtoStats.java
@@ -0,0 +1,11 @@
+package Dto;
+
+public class DtoStats {
+    public String field;
+    public Double average;
+    public Double min;
+    public Double max;
+    public Double std;
+    public Double sum;
+    public Long count;
+}
