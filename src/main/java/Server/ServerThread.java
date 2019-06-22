32d6f40199f957ccc34487c6862d2f88b868af9e
ddomen <daniele.domenichelli.5@gmail.com>
2019-06-22 11:10:53 +0200

    documentazione ServerThread

--- src/main/java/Server/ServerThread.java
+++ src/main/java/Server/ServerThread.java
@@ -6,20 +6,42 @@ import java.util.*;
 
 import Dto.*;
 
-
-
+/**
+ * Thread di gestione delle connessioni del Server
+ */
 public class ServerThread implements Runnable{
+    /**
+     * Dataset utilizzato dal server
+     */
     protected List<DtoData> dataset;
+    /**
+     * Informazioni di dataset utilizzate dal server
+     */
     protected DtoPackage dtoPackage;
+    /**
+     * Socket di connessione client - server
+     */
     protected ServerSocket socket;
+    /**
+     * Modalità verbose
+     */
     protected boolean verbose;
 
+    /**
+     * Genera la socket del server sulla porta specificata
+     * @param port porta di ascolto
+     * @throws IOException
+     */
     public ServerThread(int port) throws IOException { this.socket = new ServerSocket(port); }
 
     public ServerThread SetVerbose(boolean verbose){ this.verbose = verbose; return this; }
     public ServerThread SetData(List<DtoData> dataset){ this.dataset = dataset; return this; }
     public ServerThread SetPackage(DtoPackage dtoPackage){ this.dtoPackage = dtoPackage; return this; }
 
+    /**
+     * Metodo per l'esecuzione del thread
+     */
+    @Override
     public void run() {
         while(true){
             Connection connect = null;
